21bca1064089535a3b24238c7dfe02c4f58aa101
Daniel Roca González <dan.roca.6@gmail.com>
2022-03-02 22:35:18 +0100

    Fixed Fibonacci example

--- src/SubstitutionTilings.jl
+++ src/SubstitutionTilings.jl
@@ -1,7 +1,7 @@
 module SubstitutionTilings
 
 
-export substitute, dilate, empirical_frequency, draw, embed_aff, Tiling, SetTiling, id, vertices, in_border, transition_matrix, DGroupElem
+export substitute, dilate, empirical_frequency, draw, embed_aff, Tiling, SubSystem, id, vertices, in_border, transition_matrix, DGroupElem
 export transition_matrix
 export collar_in, is_interior, frequency, total_collaring, total_collaring, UnrecognizedCollar
 
