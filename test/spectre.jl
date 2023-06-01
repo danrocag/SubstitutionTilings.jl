f3016e759ad2d711e49975043d32d0254a1064aa
Daniel Roca González <dan.roca.6@gmail.com>
2023-06-01 09:16:30 +0200

    Tile matching

--- test/spectre.jl
+++ test/spectre.jl
@@ -60,51 +60,74 @@ end
 
 
 spectre_angles = [2, -3, 2, 0, 2, 3, -2, 3, -2, 3, 2, -3, 2, 3]
-spectre_indices = [2,4,10,14]
+spectre_indices = [2,6,12,14]
 spectre = Figure(spectre_angles, spectre_indices)
 mystic_angles = [spectre_angles[1:5];-3; spectre_angles[5:end-1]; 0; spectre_angles[11:end]]
 mystic_indices = [8,14]
 mystic = Figure(mystic_angles, mystic_indices)
 
-function displacements(fig :: Figure)
+function displacements(angles :: Vector{Int})
     displacements = G[G(K[0;0], 0)]
     current_displacement = G(K[1;0], 0)
-    for a in fig.angles
+    for a in angles
         push!(displacements, current_displacement)
         current_displacement = current_displacement*rot(a)*G(Qθ[1;0],0)
     end
     @assert current_displacement == G(K[1;0], 0)
     return [displacements[end];displacements[2:end]]
 end
+function displacements(fig :: Figure)
+    displacements(fig.angles)
+end
+
+function points(displacements :: Vector{G})
+    result = Vector{Qθ}[]
+    for g in displacements
+        push!(result, g.t)
+    end
+    result
+end
+function points(fig :: Figure)
+    points(displacements(fig))
+end
+function directions(displacements :: Vector{G})
+    result = Int[]
+    for g in displacements
+        push!(result, g.k)
+    end
+    result
+end
+function directions(figure :: Figure)
+    directions(displacements(figure))
+end
+
 
 function match(f :: Figure, i :: Int, g :: Figure, j::Int)
-    df = dirs(f)
-    pf = points(df)
-    dg = dirs(g)
-    pg = points(dg)
+    pf = displacements(f)
+    pg = displacements(g)
     ix = f.marks[i]
     jx = g.marks[j]
-    
+    arrow_f = G(pf[ix-1].t, pf[ix].k)
+    arrow_g = G(pg[jx+1].t, mod(6+pg[jx+1].k,12))
+    arrow_f*inv(arrow_g)
 end
 
 @draw begin
     f1 = mystic
     p1 = displacements(f1)
-    checks1 = p1[f1.marks]
-    #f2 = spectre
-    #p2 = points(f2)
+    f2 = spectre
+    p2 = points(f2)
     #checks2 = p2[f2.marks]
     scale(40)
     poly(Point.(p1), action=:stroke)
-    for check in checks1
-        circle(Point(check), 0.1, action=:fillstroke)
-        arrow(Point(check), Point(check*G(K[1;0],0));arrowheadlength=0.3)
+    for mark in f1.marks
+        h = G(points(f1)[mark], mod(6+directions(f1)[mark],12))
+        arrow(Point(h), Point(h*G(K[1;0],0));arrowheadlength=0.3)
     end
-    #g = match(f1,1,f2,1)
-    #transform(g)
-    #poly(Point.(p2), action=:stroke)
+    displ1 = match(f1,1,f2,2)
+    p2t = Ref(displ).*p2
+    poly(Point.(p2t), action=:stroke)
     #for check in checks2
     #    circle(Point(check), 0.1, action=:fillstroke)
     #end
-
 end 800 800
