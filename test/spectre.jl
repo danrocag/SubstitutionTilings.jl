432ff651e513ddccfb70f2396334da13a53872cd
Daniel Roca González <daniel.roca@kit.edu>
2023-06-05 16:34:32 +0200

    changes

--- test/spectre.jl
+++ test/spectre.jl
@@ -60,7 +60,7 @@ end
 
 
 spectre_angles = [2, -3, 2, 0, 2, 3, -2, 3, -2, 3, 2, -3, 2, 3]
-spectre_indices = [2,6,12,14]
+spectre_indices = [2,6,12,14,11,5]
 spectre = Figure(spectre_angles, spectre_indices)
 mystic_angles = [spectre_angles[1:5];-3; spectre_angles[5:end-1]; 0; spectre_angles[11:end]]
 mystic_indices = [8,14]
@@ -73,7 +73,7 @@ function displacements(angles :: Vector{Int})
         push!(displacements, current_displacement)
         current_displacement = current_displacement*rot(a)*G(Qθ[1;0],0)
     end
-    @assert current_displacement == G(K[1;0], 0)
+    #@assert current_displacement == G(K[1;0], 0)
     return [displacements[end];displacements[2:end]]
 end
 function displacements(fig :: Figure)
@@ -107,27 +107,127 @@ function match(f :: Figure, i :: Int, g :: Figure, j::Int)
     pg = displacements(g)
     ix = f.marks[i]
     jx = g.marks[j]
-    arrow_f = G(pf[ix-1].t, pf[ix].k)
-    arrow_g = G(pg[jx+1].t, mod(6+pg[jx+1].k,12))
+    arrow_f = G(pf[ix].t, pf[ix+1].k)
+    arrow_g = G(pg[jx].t, mod(6+pg[jx].k,12))
     arrow_f*inv(arrow_g)
 end
 
-@draw begin
-    f1 = mystic
-    p1 = displacements(f1)
-    f2 = spectre
-    p2 = points(f2)
-    #checks2 = p2[f2.marks]
-    scale(40)
-    poly(Point.(p1), action=:stroke)
-    for mark in f1.marks
-        h = G(points(f1)[mark], mod(6+directions(f1)[mark],12))
-        arrow(Point(h), Point(h*G(K[1;0],0));arrowheadlength=0.3)
+function build_supertile(init_figure :: Figure, next :: Vector{Tuple{Int, Int, Figure}})
+    #@assert init_figure == next[end][3]
+    current_figure = init_figure
+    result = Tuple{G, Figure}[]
+    g = G(K[0;0],0)
+    for (i,j,next_figure, _) in next
+        g = g*match(current_figure, i, next_figure, j)
+        push!(result, (g, next_figure))
+        current_figure = next_figure
     end
-    displ1 = match(f1,1,f2,2)
-    p2t = Ref(displ).*p2
-    poly(Point.(p2t), action=:stroke)
-    #for check in checks2
-    #    circle(Point(check), 0.1, action=:fillstroke)
-    #end
+    #@assert g == G(K[0;0],0)
+    return result
+end
+
+function supertile_instructions(spectre, mystic)
+    spectre_supertile_instructions = [
+        (2,3,spectre, 4),
+        (2,4,spectre, nothing),
+        (1,3,spectre, nothing),
+        (2,4,spectre, 1),
+        (2,4,spectre, nothing),
+        (1,3,spectre, 4),
+        (2,4,spectre, 1),
+        (2,1,mystic, nothing)
+    ]
+    mystic_supertile_instructions = [
+        (2,3,spectre, 4),
+        (2,4,spectre, nothing),
+        (6,5,spectre, 1),
+        (2,4,spectre, nothing),
+        (1,3,spectre, 4),
+        (2,4,spectre, 1),
+        (2,1,mystic, nothing)
+    ]
+    return (spectre_supertile_instructions, mystic_supertile_instructions)
+end
+
+function spectre_supertile_instructions(spectre, mystic)
+    supertile_instructions(spectre, mystic)[1]
+end
+function mystic_supertile_instructions(spectre, mystic)
+    supertile_instructions(spectre, mystic)[2]
+end
+
+@draw begin
+    scale(20)
+    #poly(Point.(displacements(mystic)), action=:stroke)
+    for (g,figure) in build_supertile(mystic, spectre_supertile_instructions)
+        ps = Ref(g).*displacements(figure)
+        poly(Point.(ps), action=:stroke)
+        setopacity(0.5)
+        for mark in figure.marks
+            circle(Point(ps[mark]), 0.2, action=:fill)
+        end
+        setopacity(1)
+    end
+end 800 800
+    
+function outline(instructions)
+    angles = Int[]
+    marks = []
+    last_connecting_mark = 1
+    (i,j, figure, new_mark) = instructions[1]
+    println(figure)
+    jx = figure.marks[j]
+    current_angles = circshift(figure.angles, 1-jx)
+    last_connecting_angle = current_angles[end]
+    last_figure = figure
+    if !isnothing(new_mark)
+        println("Marks:")
+        println(last_connecting_mark)
+        println(mod(figure.marks[new_mark]-jx, length(figure.angles)+1))
+        push!(marks, last_connecting_mark+mod(figure.marks[new_mark]-jx, length(figure.angles)))
+    end
+    for (i,j,figure, new_mark) in instructions[2:end]
+        ix = last_figure.marks[i]
+        slice = 1:mod(ix-jx,length(current_angles))-1
+        append!(angles, current_angles[slice])
+        last_connecting_mark += length(slice)+1
+        connecting_angle_i = current_angles[mod(ix-jx,length(current_angles))]
+        jx = figure.marks[j]
+        current_angles = circshift(figure.angles, 1-jx)
+        connecting_angle_j = current_angles[end]
+        push!(angles, connecting_angle_i+connecting_angle_j-6)
+        last_figure = figure
+        if !isnothing(new_mark)
+            println("Marks:")
+            println(last_connecting_mark)
+            println(mod(figure.marks[new_mark]-jx, length(figure.angles)+1))
+            push!(marks, last_connecting_mark+mod(figure.marks[new_mark]-jx, length(figure.angles)))
+        end
+    end
+    ix = last_figure.marks[i]
+    slice = 1:mod(ix-jx,length(current_angles))-1
+    append!(angles, current_angles[slice])
+    connecting_angle_i = current_angles[mod(ix-jx,length(current_angles))]
+    push!(angles, connecting_angle_i+last_connecting_angle-6)
+    return Figure(angles, marks)
+end
+
+function iterate(figures)
+    (spectre, mystic) = figures
+    superspectre_am = outline(spectre)
+end
+
+@draw begin
+    instrs = supertile_instructions(spectre, mystic)
+    (spectre2, mystic2) = outline.(instrs)
+    figure = outline(spectre_supertile_instructions(spectre, mystic))
+    println(figure)
+
+    scale(20)
+    poly(Point.(displacements(figure)), action=:stroke)
+    ds = displacements(figure)
+    for mark in figure.marks
+        circle(Point(ds[mark]), 0.15, action=:fill)
+    end
+    circle(Point(0,0),0.2,action=:fill)
 end 800 800
